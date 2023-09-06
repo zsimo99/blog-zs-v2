bfbf5e8d86008631818edd60fb87d50da60b0563
zsimo <mohamedzoraa100@gmail.com>
2023-09-06 10:03:53 +0100

    fixes

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,19 +1,10 @@
-"use client"
-import React from 'react'
-import { redirect } from 'next/navigation'
-import Profile from '@/components/Profile'
-import { useSession } from 'next-auth/react'
+import Dash from "@/components/Dash"
+
 
 const page = () => {
-    const { data: session, status } = useSession()
-    if (status && status === "unauthenticated") return redirect("/dashboard/auth")
-
-
-    return (
-        <div className='grid place-items-center md:min-h-screen max-md:min-h-[700px] max-md:h-screen relative '>
-            {session && <Profile user={session?.user} />}
-        </div>
-    )
+    return <>
+        <Dash />
+    </>
 }
 
 export default page
