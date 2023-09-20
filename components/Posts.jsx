b0e1ed31ff0015d49147cf5f43337641a1855e53
zsimo <mohamedzoraa100@gmail.com>
2023-09-20 12:08:18 +0100

    test

--- components/Posts.jsx
+++ components/Posts.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import Post from './cards/Post'
-import { Metronome } from '@uiball/loaders'
-import { useDarkMode } from '@/context/ThemeContext'
 import Loader from './Loader'
 import { v4 as uuidV4 } from 'uuid'
 import Pagination from './Pagination'
@@ -12,9 +10,9 @@ const Posts = ({ posts, length }) => {
     return (
         <>
             <h1>Result:{length}</h1>
-            <Loader />
+            {/* <Loader />
             {posts?.map((post, id) => <Post key={uuidV4()} post={post} />)}
-            <Pagination length={length} />
+            <Pagination length={length} /> */}
         </>
     )
 }
