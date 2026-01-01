c3c87d5a3aa9906a3a9ae3ec80cd02b93ab6428e
Mohamed Zoraa <mohamedzoraa100@gmail.com>
2026-01-01 12:47:04 +0100

    done

--- app/blog/[id]/page.jsx
+++ app/blog/[id]/page.jsx
@@ -14,15 +14,29 @@ import Image from 'next/image'
 export async function generateMetadata({ params }) {
   const { id } = params
   await startDB()
-
   const post = await PostModel.findById(id)
   if (!post) return notFound()
 
+  const description = post.detail.replace(/<[^>]+>/g, '').slice(0, 160) // remove all HTML tags
+
   return {
     title: post.title,
-    description: post.detail.slice(0, 160).replace("<p>","").replace("</p>",""), // first 160 chars for SEO
+    description,
+    openGraph: {
+      title: post.title,
+      description,
+      url: `${process.env.PUBLIC_NEXT_URL}/blog/${id}`,
+      images: post.image ? [post.image] : [], // optional
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: post.title,
+      description,
+      images: post.image ? [post.image] : [],
+    },
   }
 }
+
 
 
 const page = async ({ params }) => {
